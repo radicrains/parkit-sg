ed9b6cabf31d504b76bde3ec42114b4854b380b7
Juliana <juliana.mohamedali@gmail.com>
2021-04-01 12:25:33 +0800

    comments CREATE route completed

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,7 +2,16 @@ const express = require('express');
 const comment = express.Router();
 const Comments = require('../models/comments');
 
-
+//CREATE
+comment.post('/', (req, res) => {
+    Comments.create(req.body, (err, createdComments) => {
+        if (err) console.log(err.message);
+        if (createdComments) {
+            console.log(createdComments);
+            res.json(createdComments);
+        }
+    });
+});
 
 
 module.exports = comment;
