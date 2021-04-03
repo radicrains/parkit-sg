1d7ddbe3615f6e0768f429642f327fa1c0ade135
Juliana <juliana.mohamedali@gmail.com>
2021-04-03 20:02:40 +0800

    propogate comment component info from display component

--- client/src/Display.js
+++ client/src/Display.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CarparkSlots from './CarparkSlots'
+import Comments from './Comments'
 
 class Display extends React.Component {
     render() {
@@ -7,7 +8,10 @@ class Display extends React.Component {
             <div>
                 {this.props.area.map((carpark, index) => {
                     return(
-                        <div key={carpark.car_park_no} id={carpark.car_park_no}>{carpark.address} <CarparkSlots details={this.props.detail} carParkNo={carpark.car_park_no}/></div>
+                        <div key={carpark.car_park_no} id={carpark.car_park_no}>{carpark.address} 
+                        <CarparkSlots details={this.props.detail} carParkNo={carpark.car_park_no}/>
+                        <Comments comments={this.props.comments} carParkNo={carpark.car_park_no}/>
+                        </div>
                     )
                 })}
             </div>
