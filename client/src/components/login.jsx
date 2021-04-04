3bc332a1d8a1ec0496067b86a5bb65152ba0e276
danielting <daniel96legolas@gmail.com>
2021-04-05 01:42:59 +0800

    login css add picture

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LeftCar from '../components/image/left_blue_car.png';
 
 export class Login extends React.Component {
   constructor(props) {
@@ -10,19 +11,34 @@ export class Login extends React.Component {
       <div className='base-container'>
         <div className='header'>Login</div>
         <div className='content'>
+          <div className='image'>
+            <img src={LeftCar} />
+          </div>
           <div className='form'>
             <div className='form-group'>
-              <label htmlFor="login-username">Username</label>
-              <input type='text' id="login-username" name='username' placeholder='username' onChange={this.handleChange}/>
+              <label htmlFor='login-username'>Username</label>
+              <input
+                type='text'
+                id='login-username'
+                name='username'
+                placeholder='username'
+                onChange={this.handleChange}
+              />
             </div>
             <div className='form-group'>
-              <label htmlFor="login-password">Password</label>
-              <input type='password' id="login-password" name='password' placeholder='password' onChange={this.handleChange}/>
+              <label htmlFor='login-password'>Password</label>
+              <input
+                type='password'
+                id='login-password'
+                name='password'
+                placeholder='password'
+                onChange={this.handleChange}
+              />
             </div>
           </div>
         </div>
         <div className='footer'>
-          <button type='submit' className='btn' value="Login">
+          <button type='submit' className='btn' value='Login'>
             Login
           </button>
         </div>
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RightCar from '../components/image/right_car.png';
 
 export class Register extends React.Component {
   constructor(props) {
@@ -10,20 +11,35 @@ export class Register extends React.Component {
       <div className='base-container'>
         <div className='header'>Register</div>
         <div className='content'>
+          <div className='image'>
+            <img src={RightCar} />
+          </div>
           <div className='form'>
             <div className='form-group'>
-              <label htmlFor="create-username">Username</label>
-              <input type='text' id="create-username" name='username' placeholder='username' onChange={this.handleChange}/>
+              <label htmlFor='create-username'>Username</label>
+              <input
+                type='text'
+                id='create-username'
+                name='username'
+                placeholder='username'
+                onChange={this.handleChange}
+              />
             </div>
 
             <div className='form-group'>
-              <label htmlFor="create-password">Password</label>
-              <input type='text' id="login-password" name='password' placeholder='password' onChange={this.handleChange}/>
+              <label htmlFor='create-password'>Password</label>
+              <input
+                type='text'
+                id='login-password'
+                name='password'
+                placeholder='password'
+                onChange={this.handleChange}
+              />
             </div>
           </div>
         </div>
         <div className='footer'>
-          <button type='submit' className='btn' value="Create User">
+          <button type='submit' className='btn' value='Create User'>
             Register
           </button>
         </div>
